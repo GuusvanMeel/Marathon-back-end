48bb66416e7e750d41015e212b05785f265fd882
GuusvanMeel <vanmeelguus@gmail.com>
2026-03-09 11:48:01 +0100

    init

---
+++ src/main/java/com/example/Marathon/MarathonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Marathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarathonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarathonBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Marathon/backend/MarathonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Marathon.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarathonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
