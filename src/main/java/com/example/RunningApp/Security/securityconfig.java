5a6c4f63fc2cfd81d871b5d73f993fe7591c01df
GuusvanMeel <vanmeelguus@gmail.com>
2026-06-01 13:16:37 +0200

    Update securityconfig.java

--- src/main/java/com/example/RunningApp/Security/securityconfig.java
+++ src/main/java/com/example/RunningApp/Security/securityconfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+        http   
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
