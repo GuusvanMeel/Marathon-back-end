f2919e8f7f8180d075cbd56abc8e118b256c6fec
GuusvanMeel <vanmeelguus@gmail.com>
2026-06-01 14:59:22 +0200

    Update SecurityConfig.java

--- src/main/java/com/example/RunningApp/Security/SecurityConfig.java
+++ src/main/java/com/example/RunningApp/Security/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/login", "/users/hash").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
